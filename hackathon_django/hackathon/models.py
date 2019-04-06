3a4cab36736fe5c8982b68545dc91689d8e13342
Alsia Plybeah <l.a.plybeah@gmail.com>
2019-04-06 16:40:22 -0400

    picture added, commit before data

--- hackathon_django/hackathon/models.py
+++ hackathon_django/hackathon/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+# class Contract(models.Model):
+#     keyword = models.Charfield(max_length=100)
+#     # post_date = models.
+#     def __str__(self):
+#         return self.keyword
--- hackathon_django/hackathon/views.py
+++ hackathon_django/hackathon/views.py
@@ -7,6 +7,20 @@ def homepage(request):
 def search_page(request):
     return render(request, "search.html")
 
+def redirect_search(request):
+    # response = redirect(r"/search/<int:id>")
+    # return response
+    if request.method == 'POST': 
+        form = SearchForm(request.POST) 
+        if form.is_valid():  
+           keyword= form.cleaned_data['keyword']
+           post_date=form.cleaned_data['post_date']
+           set_aside=form.cleaned_data['set_aside']
+           return redirect('search/<int:id>')  
+    else: 
+            form = ContactForm()
+    return render(request, 'search_result.html', {'form': form})
+
 def search_result(request, id):
     return render(request, "search_result.html")
     #write a redirect function with the POST from the form
